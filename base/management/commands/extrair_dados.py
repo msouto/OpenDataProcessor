4c72683b6322f346eb130677b6f28246efd2e427
Allyson Barros <allysonbarrosrn@gmail.comm>
2017-05-02 15:51:41 -0300

    Criando o recurso no CKAN sempre que for realizada uma extração.

--- base/management/commands/extrair_dados.py
+++ base/management/commands/extrair_dados.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.core.management.base import BaseCommand, CommandError
-from django.core.cache import cache
-from base.models import ConfiguracaoSistema, ConjuntoDeDados, RegistroExtracao
-import requests
+
 import datetime
+import json
+
+import requests
+from ckanapi.remoteckan import RemoteCKAN
+from django.core.cache import cache
+from django.core.management.base import BaseCommand, CommandError
+
+from base.models import ConfiguracaoSistema, ConjuntoDeDados, RegistroExtracao
+from base.utils import WriteDictToCSV
+
 
 class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
@@ -22,65 +29,93 @@ class Command(BaseCommand):
                 token = None
                 conjunto_dados = ConjuntoDeDados.objects.get(pk=conjunto_dados_id)
                 configuracao_sistema = ConfiguracaoSistema.objects.first()
+                chave_cache = 'dados_extraidos_%s' % conjunto_dados.slug
             except ConjuntoDeDados.DoesNotExist:
                 raise CommandError('O Conjunto de Dados "%s" não existe.' % conjunto_dados_id)
             except ConfiguracaoSistema.DoesNotExist:
                 raise CommandError('É necessário definir as Configurações do Sistema.')
 
-            # Obtendo o Token de Autenticação
-            params = {
-                'username': configuracao_sistema.usuario_suap,
-                'password': configuracao_sistema.senha_suap,
-            }
-            print '=> Obtendo o Token de Autenticação da API do SUAP'
-            requisicao = requests.post(configuracao_sistema.url_suap + 'api/v2/autenticacao/token/', data=params)
-            if requisicao.status_code == requests.codes.ok:
-                token = requisicao.json().get('token')
-                self.stdout.write(self.style.SUCCESS('=> Token: %s\n' % token))
-            else:
-                raise CommandError('Não foi possível obter o token de autenticação da API do SUAP.\nRetorno da API do SUAP: %s' % requisicao.json())
+            if not cache.get(chave_cache):
+                # Obtendo o Token de Autenticação
+                params = {
+                    'username': configuracao_sistema.usuario_suap,
+                    'password': configuracao_sistema.senha_suap,
+                }
+                print '=> Obtendo o Token de Autenticação da API do SUAP'
+                requisicao = requests.post(configuracao_sistema.url_suap + 'api/v2/autenticacao/token/', data=params)
+                if requisicao.status_code == requests.codes.ok:
+                    token = requisicao.json().get('token')
+                    self.stdout.write(self.style.SUCCESS('=> Token: %s\n' % token))
+                else:
+                    raise CommandError('Não foi possível obter o token de autenticação da API do SUAP.\nRetorno da API do SUAP: %s' % requisicao.json())
 
-            # Executando a extração dos dados da API do SUAP.
-            url = configuracao_sistema.url_suap + conjunto_dados.url_endpoint
-            headers = {
-                'Authorization':'JWT %s' % token
-            }
+                # Executando a extração dos dados da API do SUAP.
+                url = configuracao_sistema.url_suap + conjunto_dados.url_endpoint
+                headers = {
+                    'Authorization':'JWT %s' % token
+                }
 
-            try:
-                while(True):
-                    print '=> Acessando a URL: %s' % url
-                    requisicao = requests.get(url, headers=headers)
-                    if requisicao.status_code == requests.codes.ok:
-                        json = requisicao.json()
-                        results = json.get('results')
-                        dados.extend(results)
-                        self.stdout.write(self.style.SUCCESS('=> %s dados extraídos.' % len(results)))
+                try:
+                    while(True):
+                        print '=> Acessando a URL: %s' % url
+                        requisicao = requests.get(url, headers=headers)
+                        if requisicao.status_code == requests.codes.ok:
+                            retorno_json = requisicao.json()
+                            results = retorno_json.get('results')
+                            dados.extend(results)
+                            self.stdout.write(self.style.SUCCESS('=> %s dados extraídos.' % len(results)))
 
-                        if not json.get('next'):
-                            break
+                            if not retorno_json.get('next'):
+                                break
+                            else:
+                                url = retorno_json.get('next')
                         else:
-                            url = json.get('next')
-                    else:
-                        raise CommandError('Não foi possível acessar o endpoint do conjunto de dados.\nRetorno da API do SUAP: %s' % requisicao.json())
-            except:
-                RegistroExtracao.objects.create(
-                    conjunto_dados=conjunto_dados,
-                    data_horario_inicio_extracao=data_inicio_extracao,
-                    data_horario_termino_extracao=datetime.datetime.now(),
-                    quantidade_registros=0,
-                    status=RegistroExtracao.STATUS_FALHA
+                            raise CommandError('Não foi possível acessar o endpoint do conjunto de dados.\nRetorno da API do SUAP: %s' % requisicao.json())
+                except:
+                    RegistroExtracao.objects.create(
+                        conjunto_dados=conjunto_dados,
+                        data_horario_inicio_extracao=data_inicio_extracao,
+                        data_horario_termino_extracao=datetime.datetime.now(),
+                        quantidade_registros=0,
+                        status=RegistroExtracao.STATUS_FALHA
+                    )
+                    raise CommandError('Não foi possível acessar o endpoint do conjunto de dados.')
+
+
+                # Exibindo a quantidade de dados extraídos
+                self.stdout.write(self.style.SUCCESS('\n=> %s dados extraídos com sucesso.' % len(dados)))
+
+                # Fazendo cache dos dados extraídos
+                cache.set(chave_cache, dados)
+                self.stdout.write(self.style.SUCCESS('=> Os dados extraídos foram inseridos no cache através da chave "%s".' % chave_cache))
+            else:
+                self.stdout.write(self.style.SUCCESS('=> Os dados extraídos foram lidos do cache através da chave "%s".' % chave_cache))
+                dados = cache.get(chave_cache)
+
+            # Salvando o arquivo JSON para carregar no CKAN.
+            filename = '/tmp/dados_extraidos_%s.json' % conjunto_dados.slug
+            with open(filename, 'w') as arquivo_json:
+                json.dump(dados, arquivo_json)
+
+            # Enviando o arquivo para o CKAN
+            ckan = RemoteCKAN(configuracao_sistema.url_ckan, apikey=configuracao_sistema.token_ckan)
+            retorno_json = ckan.action.package_show(id=conjunto_dados.id_ckan)
+
+            if retorno_json.get('num_resources') == 0:
+                ckan.action.resource_create(
+                    package_id=conjunto_dados.id_ckan,
+                    name=conjunto_dados.titulo,
+                    url=conjunto_dados.slug,
+                    upload=open(filename, 'rb')
                 )
-                raise CommandError('Não foi possível acessar o endpoint do conjunto de dados.')
-
-
-            # Exibindo a quantidade de dados extraídos
-            self.stdout.write(self.style.SUCCESS('\n=> %s dados extraídos com sucesso.' % len(dados)))
-
-            # Fazendo cache dos dados extraídos
-            chave_cache = 'dados_extraidos_%s' % conjunto_dados.slug
-            cache.set(chave_cache, dados)
-            self.stdout.write(self.style.SUCCESS('=> Os dados extraídos foram inseridos no cache através da chave "%s".' % chave_cache))
-
+            else:
+                id_recurso = retorno_json.get('resources')[0].get('id')
+                ckan.action.resource_update(
+                    id=id_recurso,
+                    name=conjunto_dados.titulo,
+                    url=conjunto_dados.slug,
+                    upload=open(filename, 'rb')
+                )
             # Salvando o Registro de Execução da Extração.
             RegistroExtracao.objects.create(
                 conjunto_dados=conjunto_dados,
---
+++ base/utils.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import unicodecsv as csv
+
+
+def WriteDictToCSV(csv_file, csv_columns, dict_data):
+    try:
+        with open(csv_file, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer.writeheader()
+            for data in dict_data:
+                writer.writerow(data)
+    except IOError as (errno, strerror):
+        print("I/O error({0}): {1}".format(errno, strerror))
