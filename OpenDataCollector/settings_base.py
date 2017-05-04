036f36caf9aa74d851a7798b3644500863283ba8
Allyson Barros <allysonbarrosrn@gmail.comm>
2017-05-04 15:23:37 -0300

    Adicionando os arquivos de template para deploy no VPS.

--- OpenDataCollector/settings_base.py
+++ OpenDataCollector/settings_base.py
@@ -3,8 +3,11 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from os.path import join, dirname, abspath
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_PATH = abspath(dirname(dirname(__file__)))
+PROJECT_NAME = PROJECT_PATH.split('/')[-1]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -110,4 +113,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = join(PROJECT_PATH, 'deploy/static')
 STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
